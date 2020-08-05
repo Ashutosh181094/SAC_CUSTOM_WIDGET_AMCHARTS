ea2a0ce3645a03c1c58d081d1728d40cb10336b4
Ashutosh <ashutoshrai181094@gmail.com>
2020-08-06 00:22:20 +0530

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -1,24 +1,99 @@
 (function()  {
-    let tmpl = document.createElement('template');
-    tmpl.innerHTML = `
-      <h1>Hello World</h1>
+    
+    const amchartscorejs = "https://cdn.amcharts.com/lib/4/core.js";
+    const amchartschartsjs = "https://cdn.amcharts.com/lib/4/charts.js";
+    const amchartsanimatedjs = "https://cdn.amcharts.com/lib/4/themes/animated.js"
+    
+    console.log("1-Step");
+    
+    //This function is used to load the library
+    
+    function loadScript(src)
+    {
+    
+    console.log("Step-9");
+    return new Promise(function(resolve, reject) 
+    {
+		let script = document.createElement('script');
+		script.src = src;
+
+		script.onload = () => {console.log("Load: " + src); resolve(script);}
+		script.onerror = () => reject(new Error(`Script load error for ${src}`));
+
+		document.head.appendChild(script)
+	  });
+     }
+    
+    
+    console.log("Step-2");
+    
+    let template = document.createElement('template');
+    template.innerHTML = `
+    <style>
+    #chartdiv {
+            width: 100%;
+            height:500px;
+    }
+    </style>
+    <div id="chartdiv"></div>
     `;
+  
+    console.log("Step3");
 
-    customElements.define('com-sap-sample-template', class WidgetTemplate extends HTMLElement {
-
-
-		constructor() {
-			super(); 
-			let shadowRoot = this.attachShadow({mode: "open"});
-			shadowRoot.appendChild(tmpl.content.cloneNode(true));
-		}
-
+    
+   
+  
+  customElements.define('com-sap-sample-helloworld5', class HelloWorld extends HTMLElement     {
+   
+constructor() {
+     super(); 
+     console.log("step-4");
+     let shadowRoot = this.attachShadow({mode: "open"});
+     shadowRoot.appendChild(template.content.cloneNode(true));
+     this._firstConnection = false;
+     this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+            });
+}
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
-            this._firstConnection = true;
-            this.redraw();
-        }
+        
+        console.log("Step-5");
+        this._firstConnection = true;
+        
+        async function LoadLibs(callme) {
+        console.log("Step - 7");
+        
+	try
+        {
+	console.log("Step-8");
+	await loadScript(amchartscorejs);				
+	await loadScript(amchartschartsjs);		
+	await loadScript(amchartsanimatedjs);
+            
+	} 
+        catch (e) 
+        {
+	 alert(e);
+	} 
+        finally 
+        {
+        console.log("Step-10");
+        console.log(" execute kyun nahi ho raha");
+        callme.redraw();
+	}
+    }
+        
+        console.log("Step-6");
+        LoadLibs(this);
+}
+        
+       
+       
+       
+
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
         disconnectedCallback(){
@@ -26,34 +101,163 @@
         }
 
          //When the custom widget is updated, the Custom Widget SDK framework executes this function first
-		onCustomWidgetBeforeUpdate(oChangedProperties) {
-
-		}
+        onCustomWidgetBeforeUpdate(oChangedProperties) {
+        }
+    
+    
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
-		onCustomWidgetAfterUpdate(oChangedProperties) {
+	onCustomWidgetAfterUpdate(oChangedProperties) {
             if (this._firstConnection){
-                this.redraw();
+            this.redraw();
             }
         }
         
         //When the custom widget is removed from the canvas or the analytic application is closed
         onCustomWidgetDestroy(){
-        }
-
-        //When the custom widget is resized on the canvas, the Custom Widget SDK framework executes the following JavaScript function call on the custom widget
-        // Commented out by default.  If it is enabled, SAP Analytics Cloud will track DOM size changes and call this callback as needed
-        //  If you don't need to react to resizes, you can save CPU by leaving it uncommented.
-        /*
-        onCustomWidgetResize(width, height){
         
-        }
-        */
+	}
 
-        redraw(){
+        /*
+	 get chartType() {
+            return this.chartTypeValue;
+        }
+
+        set chartType(value) {
+            this.chartTypeValue = value;
+        }
+        
+     */
+        
+
+
+      redraw()
+        {
+        let myChart=this.shadowRoot.getElementById('chartdiv');
+        console.log("Step-11");
+        am4core.useTheme(am4themes_animated);
+	var chart = am4core.create(myChart, am4charts.GaugeChart);
+        chart.hiddenState.properties.opacity = 0;
+	var axis = chart.xAxes.push(new am4charts.ValueAxis());
+        axis.min = 0;
+        axis.max = 100;
+        axis.strictMinMax = true;
+        axis.renderer.inside = true;
+        axis.renderer.radius = am4core.percent(80);
+        axis.renderer.line.strokeOpacity = 1;
+        axis.renderer.line.strokeWidth = 5;
+        axis.renderer.line.stroke = chart.colors.getIndex(0);
+        axis.renderer.ticks.template.disabled = false
+        axis.renderer.ticks.template.stroke = chart.colors.getIndex(0);
+        axis.renderer.labels.template.radius = 35;
+        axis.renderer.ticks.template.strokeOpacity = 1;
+        axis.renderer.grid.template.disabled = true;
+        axis.renderer.ticks.template.length = 10;
+        axis.hiddenState.properties.opacity = 1;
+        axis.hiddenState.properties.visible = true;
+        axis.setStateOnChildren = true;
+        axis.renderer.hiddenState.properties.endAngle = 180;
+
+       var axis2 = chart.xAxes.push(new am4charts.ValueAxis());
+       axis2.min = 0;
+       axis2.max = 240;
+       axis2.strictMinMax = true;
+       axis2.renderer.line.strokeOpacity = 1;
+       axis2.renderer.line.strokeWidth = 5;
+       axis2.renderer.line.stroke = chart.colors.getIndex(3);
+       axis2.renderer.ticks.template.stroke = chart.colors.getIndex(3);
+       axis2.renderer.ticks.template.disabled = false
+       axis2.renderer.ticks.template.strokeOpacity = 1;
+       axis2.renderer.grid.template.disabled = true;
+       axis2.renderer.ticks.template.length = 10;
+       axis2.hiddenState.properties.opacity = 1;
+       axis2.hiddenState.properties.visible = true;
+       axis2.setStateOnChildren = true;
+       axis2.renderer.hiddenState.properties.endAngle = 180;
+
+       var hand = chart.hands.push(new am4charts.ClockHand());
+       hand.fill = axis.renderer.line.stroke;
+       hand.stroke = axis.renderer.line.stroke;
+       hand.axis = axis;
+       hand.pin.radius = 14;
+       hand.startWidth = 10;
+
+       var hand2 = chart.hands.push(new am4charts.ClockHand());
+       hand2.fill = axis2.renderer.line.stroke;
+       hand2.stroke = axis2.renderer.line.stroke;
+       hand2.axis = axis2;
+       hand2.pin.radius = 10;
+       hand2.startWidth = 10;
+
+      setInterval(function() {
+      hand.showValue(20/100 * 160, 1000, am4core.ease.cubicOut);
+      label.text = Math.round(hand.value).toString();
+      hand2.showValue(40/100 * 160, 1000, am4core.ease.cubicOut);
+      label2.text = Math.round(hand2.value).toString();
+      }, 5000);
+
+      var legend = new am4charts.Legend();
+      legend.isMeasured = false;
+      legend.y = am4core.percent(100);
+      legend.verticalCenter = "bottom";
+      legend.parent = chart.chartContainer;
+      legend.data = [{
+      "name": "Measurement #1",
+      "fill": chart.colors.getIndex(0)
+      }, {
+      "name": "Measurement #2",
+      "fill": chart.colors.getIndex(3)
+       }];
+
+     legend.itemContainers.template.events.on("hit", function(ev) {
+     var index = ev.target.dataItem.index;
+
+     if (!ev.target.isActive) {
+     chart.hands.getIndex(index).hide();
+     chart.xAxes.getIndex(index).hide();
+     labelList.getIndex(index).hide();
+     }
+	     
+    else 
+    {
+    chart.hands.getIndex(index).show();
+    chart.xAxes.getIndex(index).show();
+    labelList.getIndex(index).show();
+  }
+});
+
+   var labelList = new am4core.ListTemplate(new am4core.Label());
+   labelList.template.isMeasured = false;
+   labelList.template.background.strokeWidth = 2;
+   labelList.template.fontSize = 25;
+   labelList.template.padding(10, 20, 10, 20);
+   labelList.template.y = am4core.percent(50);
+   labelList.template.horizontalCenter = "middle";
+
+  var label = labelList.create();
+  label.parent = chart.chartContainer;
+  label.x = am4core.percent(40);
+  label.background.stroke = chart.colors.getIndex(0);
+  label.fill = chart.colors.getIndex(0);
+  label.text = "0";
+
+  var label2 = labelList.create();
+  label2.parent = chart.chartContainer;
+  label2.x = am4core.percent(60);
+  label2.background.stroke = chart.colors.getIndex(3);
+  label2.fill = chart.colors.getIndex(3);
+  label2.text = "0";
+
+
+ 
+        
+        
+   
+
         }
     
     
     });
-        
+    
+   
 })();
