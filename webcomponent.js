2c542fd50569f5e3e4e5fdb20e95f259f0731b71
Ashutosh <ashutoshrai181094@gmail.com>
2020-08-12 18:31:03 +0530

    Update webcomponent.js

--- webcomponent.js
+++ webcomponent.js
@@ -67,7 +67,7 @@
 
 
 
-  customElements.define('com-sap-sample-helloworld5', class HelloWorld extends HTMLElement {
+  customElements.define('com-sap-sample-helloworld6', class HelloWorld extends HTMLElement {
     constructor() {
       super();
 
